66156fbd19ef06d42731c764a02c8ae18486226a
adimerpaul <adimer101@gmail.com>
2025-05-16 03:57:21 -0400

    Actulizado front

---
+++ front/src/pages/registro/Registro.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <q-page>
+        asa
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+<script>
+export default {
+
+}
+</script>
