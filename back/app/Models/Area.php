e2795cdc34820b260a9295bd859bc62469bce3d9
adimerpaul <adimer101@gmail.com>
2025-08-13 21:17:45 -0400

    Actulizado apra compra

--- back/app/Models/Area.php
+++ back/app/Models/Area.php
@@ -7,10 +7,35 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Area extends Model{
     use SoftDeletes;
+//$table->id();
+//$table->string('area')->nullable();
+//$table->string('curso1')->nullable();
+//$table->string('curso2')->nullable();
+//$table->string('curso3')->nullable();
+//$table->string('curso4')->nullable();
+//$table->string('curso5')->nullable();
+//$table->string('curso6')->nullable();
+//$table->string('titulo_fecha1')->nullable();
+//$table->date('fecha1')->nullable();
+//$table->string('titulo_fecha2')->nullable();
+//$table->date('fecha2')->nullable();
+//$table->text('lugar')->nullable();
+//$table->string('modalidad')->nullable();
+//$table->text('inscripcion')->nullable();
+//$table->softDeletes();
+//$table->timestamps();
     protected $fillable = [
         'area',
-        'curso',
-        'fecha',
+        'curso1',
+        'curso2',
+        'curso3',
+        'curso4',
+        'curso5',
+        'curso6',
+        'titulo_fecha1',
+        'fecha1',
+        'titulo_fecha2',
+        'fecha2',
         'lugar',
         'modalidad',
         'inscripcion',
--- back/database/migrations/2025_05_15_172605_create_areas_table.php
+++ back/database/migrations/2025_05_15_172605_create_areas_table.php
@@ -14,11 +14,19 @@ return new class extends Migration
         Schema::create('areas', function (Blueprint $table) {
             $table->id();
             $table->string('area')->nullable();
-            $table->string('curso')->nullable();
-            $table->string('fecha')->nullable();
-            $table->string('lugar')->nullable();
+            $table->string('curso1')->nullable();
+            $table->string('curso2')->nullable();
+            $table->string('curso3')->nullable();
+            $table->string('curso4')->nullable();
+            $table->string('curso5')->nullable();
+            $table->string('curso6')->nullable();
+            $table->string('titulo_fecha1')->nullable();
+            $table->date('fecha1')->nullable();
+            $table->string('titulo_fecha2')->nullable();
+            $table->date('fecha2')->nullable();
+            $table->text('lugar')->nullable();
             $table->string('modalidad')->nullable();
-            $table->string('inscripcion')->nullable();
+            $table->text('inscripcion')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
