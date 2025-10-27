b144b4433be1aa5ebc2675a5d1809c8a03d3751f
adimerpaul <adimer101@gmail.com>
2025-10-27 06:33:10 -0400

    Creado campos para slam

--- front/dist/pwa/assets/QSpace-BjpOcu2U.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:front/dist/pwa/assets/QSpace-BjpOcu2U.js
-import{j as a,l as s}from"./index-CtksMiZ2.js";const p=a({name:"QSpace",setup(){const e=s("div",{class:"q-space"});return()=>e}});export{p as Q};
-========
-import{j as a,l as s}from"./index-B95ofqa7.js";const p=a({name:"QSpace",setup(){const e=s("div",{class:"q-space"});return()=>e}});export{p as Q};
->>>>>>>> 938ae3cfe102fe30bac46559ab2318c789061504:front/dist/pwa/assets/QSpace-DjkncaFs.js
